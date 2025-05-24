00aeba70b4b049f4b7aa035f55069421481a633a
bChethan <chetanreddy500@gmail.com>
2025-05-24 16:07:32 +0530

    config

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Tic-Tac-Toe/', // Your repo name
+  base: '/Tic-Tac-Toe/', // Must match your repo name exactly
   build: {
-  outDir: 'dist',
-}
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 })
